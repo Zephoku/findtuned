6540433a721ab66232523c03d1612582d3c5519f
Robert Nguyen <roberttnb@gmail.com>
2013-04-03 14:11:18 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findtuned::Application.config.secret_token = '498c289f9df16c4c472fb517c3eb41ba72faea087ca92558d36afb9e631203b45eb4eb92cb1995aaa9d488c39fdd7ea5b0704a179efde96fae4d4d7cf591dca9'
