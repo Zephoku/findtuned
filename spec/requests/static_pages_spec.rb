de4ec911737121bffcc1b467db923b6625767140
Robert Nguyen <roberttnb@gmail.com>
2013-04-03 21:00:00 -0700

    rspec update

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1', text: 'FindTuned') }
+    #it { should have_selector('title', text: 'FindTuned') }
+  end
+end
