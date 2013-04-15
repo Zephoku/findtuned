34cff891dc6a85b436a9688bca1c717f9196415a
Robert Nguyen <roberttnb@gmail.com>
2013-04-15 11:42:40 -0700

    Implement sign up forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
-class UsersController < ApplicationController def show
-  @user = User.find(params[:id])
-end
+class UsersController < ApplicationController 
+  def show
+    @user = User.find(params[:id])
+  end
 
-def new
-  @user = User.new
-end
-def create
-  @user = User.new(params[:user])
-  if @user.save
-    flash[:success] = "Welcome to the FindTuned!"
-    redirect_to @user
-  else
-    render 'new'
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the FindTuned!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
-    end
